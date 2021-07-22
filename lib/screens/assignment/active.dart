a1f3a2b01fdf887fb00c48e132e051ac9e99e805
wajahat hussain <53147495+wajhiii@users.noreply.github.com>
2021-07-22 13:58:39 +0500

    Update active.dart

--- lib/screens/assignment/active.dart
+++ lib/screens/assignment/active.dart
@@ -8,12 +8,55 @@ class Active extends StatefulWidget {
 }
 
 class _ActiveState extends State<Active> {
+  bool _reverseSort = false;
+  final List<String> _items = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').toList();
+
+  // Handler called by ReorderableListView onReorder after a list child is
+  // dropped into a new position.
+  void _onReorder(int oldIndex, int newIndex) {
+    setState(() {
+      final newIdx = newIndex > oldIndex ? newIndex - 1 : newIndex;
+      final item = _items.removeAt(oldIndex);
+      _items.insert(newIdx, item);
+    });
+  }
+
+  // Handler called when the "sort" button on appbar is clicked.
+  void _onSort() {
+    setState(() {
+      _reverseSort = !_reverseSort;
+      _items.sort((a, b) => _reverseSort ? b.compareTo(a) : a.compareTo(b));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-     return new Scaffold(
-      body: new Center(
-        child: new Text('This is active assignment listing'),
+      final _appbar = AppBar(
+        title: const Text('Reorderable list'),
+        backgroundColor: Color(0xff5808e5),
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.sort_by_alpha),
+            tooltip: 'Sort',
+            onPressed: _onSort,
+          ),
+          
+        ],
+      );
+      return Scaffold(
+      appBar: _appbar,
+      body: ReorderableListView(
+        onReorder: _onReorder,
+        children: [
+          for (final item in _items)
+            ListTile(
+              key: Key(item),
+              title: Text('item $item'),
+            )
+        ],
       ),
     );
   }
 }
+
